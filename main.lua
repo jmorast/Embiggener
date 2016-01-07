9a39610b80d2ee9c3b4c6d0ad31d321cdc07cda2
jmorast <jmorast>
2016-01-06 23:23:08 -0500

    Box drawn

--- main.lua
+++ main.lua
@@ -4,4 +4,7 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local myRectangle = display.newRect( 100, 100, 150, 50 )
+myRectangle.strokeWidth = 3
+myRectangle:setFillColor( 0.5 )
+myRectangle:setStrokeColor( 1, 0, 0 )
