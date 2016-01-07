c090b5744488273fd807b2c6dc2eb6135763a18a
Joshua Morast <jmorast@emich.edu>
2016-01-07 09:20:45 -0500

    First touch event

--- main.lua
+++ main.lua
@@ -8,3 +8,16 @@ local myRectangle = display.newRect( 100, 100, 150, 50 )
 myRectangle.strokeWidth = 3
 myRectangle:setFillColor( 0.5 )
 myRectangle:setStrokeColor( 1, 0, 0 )
+myRectangle.id = "le box"
+
+local function onObjectTouch( event )
+    if ( event.phase == "began" ) then
+        print( "Touch event began on: " .. event.target.id )
+        event.target.x = event.target.x + 5
+    elseif ( event.phase == "ended" ) then
+        print( "Touch event ended on: " .. event.target.id )
+    end
+    return true
+end
+myRectangle:addEventListener( "touch", onObjectTouch )
+
