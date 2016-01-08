55b214a2d4148b5523e38445430681bf39324e19
Joshua Morast <jmorast@emich.edu>
2016-01-08 16:49:03 -0500

    Fixed issue with background

--- main.lua
+++ main.lua
@@ -6,26 +6,33 @@
 print( "display.contentWidth:display.contentHeight " .. display.contentWidth .. ":" .. display.contentHeight)
 print( "display.actualcontentWidth:display.contentHeight " .. display.actualContentWidth .. ":" .. display.actualContentHeight)
 print( "display.viewableContentWidth    " .. display.viewableContentWidth )
+print( "display.screenOriginX " .. display.screenOriginX)
 local myWidth = math.round(display.actualContentWidth)
 print ("myWidth " .. myWidth)
+controllerWidth=256
 
-touchLeft = display.newRect (300,300, 100, 100)
+local bg = display.newRect(0,0,10000,10000)
+bg:setFillColor( 1,.1,.1 )
+bg.x = 0
+bg.y = 0
+
+local touchLeft = display.newRect (300,300, 100, 100)
 touchLeft:setFillColor( .1,.1,.1 )
 touchLeft.id = "Touch Left"
 
 --local controller_left = display.newRect (myWidth/6, display.contentHeight/2, myWidth/2, display.contentHeight)
-local controller_left = display.newRect (math.round(myWidth/2/3), display.contentHeight/2, myWidth/2, display.contentHeight)
+local controller_left = display.newRect (display.screenOriginX + myWidth/4, display.contentHeight/2, myWidth/2, display.contentHeight)
 controller_left:setFillColor( .5,.1,.2)
 controller_left.id = "Left"
 print ( " left controller x + width : " .. controller_left.x .. " : " .. controller_left.width)
 print ( " left controller y + height : " .. controller_left.y .. " : " .. controller_left.height)
 
-local controller_right = display.newRect ((myWidth/2) + (myWidth/2/3), display.contentHeight/2, myWidth/2, display.contentHeight)
+local controller_right = display.newRect (display.screenOriginX + myWidth/2 + myWidth/4, display.contentHeight/2, myWidth/2, display.contentHeight)
 controller_right:setFillColor( .5,.2,.2)
 controller_right.id = "Right"
 print ( " right controller x + width : " .. controller_right.x .. " : " .. controller_right.width)
 
-local myRectangle = display.newRect( 500, 300, 20, 20 )
+local myRectangle = display.newRect( 0, 300, 20, 20 )
 myRectangle.strokeWidth = 3
 myRectangle:setFillColor( 0.5 )
 myRectangle:setStrokeColor( 1, 0, 0 )
