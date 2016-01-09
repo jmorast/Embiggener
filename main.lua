8802ed047682292d5544b9febb1a63e55c3b89d2
jmorast <jmorast>
2016-01-09 15:16:15 -0500

    somecrappy version

--- main.lua
+++ main.lua
@@ -6,25 +6,36 @@
 print( "display.contentWidth:display.contentHeight " .. display.contentWidth .. ":" .. display.contentHeight)
 print( "display.actualcontentWidth:display.contentHeight " .. display.actualContentWidth .. ":" .. display.actualContentHeight)
 print( "display.viewableContentWidth    " .. display.viewableContentWidth )
-local myWidth = math.round(display.actualContentWidth)
+print( "pixel width " .. display.pixelWidth)
+print ("screen origin : " ..display.screenOriginX)
+
+--local myWidth = math.round(display.viewableContentWidth)
+local myWidth = display.contentWidth - display.screenOriginX*2
+local controllerHeight = display.contentHeight
+local controllerWidth = myWidth/2
 print ("myWidth " .. myWidth)
+print ("controllerWidth " .. controllerWidth)
 
-touchLeft = display.newRect (300,300, 100, 100)
-touchLeft:setFillColor( .1,.1,.1 )
-touchLeft.id = "Touch Left"
 
+
+local bg=display.newRect(0,0,10000,10000)
+bg:setFillColor(1,1,1)
+bg.x=0
+bg.y=0
 --local controller_left = display.newRect (myWidth/6, display.contentHeight/2, myWidth/2, display.contentHeight)
-local controller_left = display.newRect (math.round(myWidth/2/3), display.contentHeight/2, myWidth/2, display.contentHeight)
-controller_left:setFillColor( .5,.1,.2)
+local controller_left = display.newRect(myWidth*.1722,0, controllerWidth, controllerHeight)
+controller_left:setFillColor( .2,.1,.2)
 controller_left.id = "Left"
-print ( " left controller x + width : " .. controller_left.x .. " : " .. controller_left.width)
-print ( " left controller y + height : " .. controller_left.y .. " : " .. controller_left.height)
+--controllerGroup:insert(controller_left)
+print ( "  left controller x + width : " .. controller_left.x .. " : " .. controller_left.width)
 
-local controller_right = display.newRect ((myWidth/2) + (myWidth/2/3), display.contentHeight/2, myWidth/2, display.contentHeight)
+
+local controller_right = display.newRect(myWidth*.718, 0, controllerWidth, controllerHeight)
 controller_right:setFillColor( .5,.2,.2)
 controller_right.id = "Right"
 print ( " right controller x + width : " .. controller_right.x .. " : " .. controller_right.width)
-
+-- iphone 5s - .673
+-- kindle 7 - .718
 local myRectangle = display.newRect( 500, 300, 20, 20 )
 myRectangle.strokeWidth = 3
 myRectangle:setFillColor( 0.5 )
