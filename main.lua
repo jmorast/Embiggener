a842dedcee1f6fec886607db5431ec3b4f5f2f6d
Rex Peppers <jmorast@gmail.com>
2016-01-10 01:12:38 -0500

    initial collision is starting to work, but not after paddle growth

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+--Set up the physics world
+local physics = require( "physics" )
+physics.start()
 -- setup variables
 local movingLeft=false
 local movingRight=false
@@ -13,6 +16,8 @@ print( "display.contentWidth:display.contentHeight " .. display.contentWidth .. 
 print( "display.actualcontentWidth:display.contentHeight " .. display.actualContentWidth .. ":" .. display.actualContentHeight)
 print( "display.viewableContentWidth    " .. display.viewableContentWidth )
 print( "display.screenOriginX " .. display.screenOriginX)
+
+
 
 local controller_left = display.newRect (display.screenOriginX + display.actualContentWidth/4, display.actualContentHeight/2, display.actualContentWidth/2, display.actualContentHeight)
 controller_left:setFillColor( .5,.1,.2)
@@ -25,19 +30,38 @@ controller_right.id = "Right"
 local playerPaddle = display.newRect( 55, 300, 80, 20 )
 playerPaddle:setFillColor( 0,1,0 )
 playerPaddle.id = "le box"
+physics.addBody(playerPaddle, "static")
 
 local function removeBox(thisBox)
     display.remove(thisBox)
     thisBox=nil
 end
 
-local function spawnBox(boxType,x,y)
+local function spawnBox(boxType,xStart,yStart)
     local box = display.newRect(50,50,25,25)
-    transition.to(box, { time=1000 , x=math.random(-10,400), y=200, onComplete=removeBox })
+    box.id = boxType
+   -- transition.to(box, { time=1000 , x=math.random(-10,400), y=300, onComplete=removeBox })
+    physics.addBody(box,"dynamic")
     if (boxType == "red") then
         box:setFillColor( 1,0,0)
     elseif (boxType == "blue") then
         box:setFillColor(0,0,1)
+    end
+end
+
+local function onCollision( self, event )
+    if ( event.phase == "began" ) then
+        if ( self.id == "limitLine") then
+            removeBox(event.other)
+        elseif ( event.other.id == "blue" ) then
+            print ("blue hit")
+            removeBox(event.other)
+            playerPaddle.width = playerPaddle.width + 10
+        end
+        print (self.id .. " : collision began with " .. event.other.id)
+    end
+    if ( event.phase == "ended") then
+        print (self.id .. " : collision began with " .. event.other.id)
     end
 end
 
@@ -84,3 +108,14 @@ local myListener = function( event )
     end
 end
 Runtime:addEventListener( "enterFrame", myListener )
+playerPaddle.collision = onCollision
+playerPaddle:addEventListener("collision", playerPaddle)
+
+
+local limitLine = display.newRect(display.actualContentWidth/2,display.actualContentHeight*2,display.actualContentWidth *2,50)
+limitLine.strokeWidth= 2
+limitLine.id = "limitLine"
+physics.addBody(limitLine, "static")
+
+limitLine.collision = onCollision
+limitLine:addEventListener("collision", limitLine)
